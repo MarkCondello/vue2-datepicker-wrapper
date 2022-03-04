246b1d8e935b3ab94d8490a753c0661db0dbc32e
Mark C <mark@dcode.com.au>
2022-03-04 17:23:37 +1100

    attempted fix formattedDate issues computed prop

--- DatepickerWrapper.vue
+++ DatepickerWrapper.vue
@@ -55,10 +55,13 @@
         data() {
             return {
                 date: this.value,
+              unconvertedDates: false,
+                counter: 0
             }
         },
         methods: {
             dateToIso(date) {
+               this.counter > 0 ? console.log('reached datetoiso', date) : null
                 return (date instanceof Date)
                     ? `${date.getFullYear()}-${this.padZero(date.getMonth() + 1)}-${this.padZero(date.getDate())}`
                     : null
@@ -71,13 +74,30 @@
             formattedDate() {
                 if (this.date) {
                     if (this.isRange) {
-                        let dateStart = new Date(this.date[0])
-                        let dateEnd = new Date(this.date[1])
-                        return [this.dateToIso(dateStart), this.dateToIso(dateEnd)]
-                    } else {
-                        let date = new Date(this.date)
-                        return this.dateToIso(date)
+                
+                    //     let start = this.date[0]
+                    //     let end = this.date[1]
+                    //    this.counter > 0 ? console.log('date', this.date, {start, end}) : null;
+
+                            //format needs to be yyyy-mm-dd to be converted to a Date
+                        let dateStart =  this.unconvertedDates ? Date(this.date[0]) : this.date[0]
+                        let dateEnd =  this.unconvertedDates ? new Date(this.date[1]) : this.date[1]
+
+                         this.counter > 0 ? console.log({dateStart, dateEnd}) : null;
+                         this.counter > 0 ? console.log(dateStart instanceof Date, dateEnd instanceof Date) : null;
+
+                        let startFormat = this.dateToIso(dateStart)
+                        let endFormat = this.dateToIso(dateEnd)
+
+                        this.counter > 0 ? console.log({startFormat, endFormat}) : null;
+                        this.unconvertedDates = false
+                        this.counter++
+                        console.log('log in formattedDate')
+                        return [startFormat, endFormat]
+                       
                     }
+                    let date = new Date(this.date)
+                    return this.dateToIso(date)
                 }
                 else {
                     return null
